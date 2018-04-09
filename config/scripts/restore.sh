e4654a3c8fdc4ba6a8d0adfa8ba77b0da92a5e5c
Maurits Lawende <mauritsl@users.noreply.github.com>
2018-04-09 10:58:11 +0200

    Check if file exists before restore

--- config/scripts/restore.sh
+++ config/scripts/restore.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 if [ -z "$SKIP_BACKUP" ]; then
-  cd /var/www/web/sites/default
-  tar -xzf /backup/files.tar.gz
+  if [ -r /backup/files.tar.gz ]; then
+    cd /var/www/web/sites/default
+    tar -xzf /backup/files.tar.gz
+  fi
 fi
