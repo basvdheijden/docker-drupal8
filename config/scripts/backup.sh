7df01449065e052182a488105421022de6e3a15a
Bas van der Heijden <info@basvdheijden.com>
2018-04-11 15:43:16 +0200

    Filter out "all" folder

--- config/scripts/backup.sh
+++ config/scripts/backup.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$SKIP_BACKUP" ]; then
   for x in `ls /var/www/web/sites`; do
-    if [ -d "/var/www/web/sites/$x" ]; then
+    if [ -f "/var/www/web/sites/$x/settings.php" ]; then
       cd /var/www/web/sites/$x
       tar -czf /backup/$x.tar.gz files
     fi
